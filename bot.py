80ecc4bfc683d38d7904b2b5935a3cb0f9283636
Mark <mark.edmondson@cellpointmobile.com>
2023-05-15 08:34:42 +0200

    first go

---
+++ bot.py
@@ -0,0 +1,29 @@
+import os
+import discord
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')  # Get your bot token from the .env file
+FLASKURL = os.getenv('FLASK_URL')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!sheep'):
+        # Forward the message content to your Flask app
+        flask_app_url = FLASKURL
+        payload = {
+            'content': message.content
+        }
+        response = requests.post(flask_app_url, json=payload)
+
+client.run(TOKEN)
